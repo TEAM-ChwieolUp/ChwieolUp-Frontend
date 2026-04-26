126484661d538dbe9694b9d9704db43af173b63e
minwoo1119 <minwoo1119@naver.com>
2026-04-26 17:39:01 +0900

    feat: 메인 페이지 UI 퍼블리싱 #3

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,6 @@
 import CountSummary from '@/components/home/count-summary/CountSummary';
+import KanbanPreview from '@/components/home/kanban-preview/KanbanPreview';
+import Timeline from '@/components/home/timeline/Timeline';
 import TodayBriefing from '@/components/home/today-briefing/TodayBriefing';
 import { dummyTodayBriefingType } from '../api/dummy/dummyTodayBriefingType';
 import styles from './page.module.scss';
@@ -8,6 +10,10 @@ export default function HomePage() {
     <div className={styles.container}>
       <TodayBriefing {...dummyTodayBriefingType} />
       <CountSummary />
+      <div className={styles.row}>
+        <KanbanPreview />
+        <Timeline />
+      </div>
     </div>
   );
 }
